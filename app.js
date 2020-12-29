43bbb880ec75fadf3a4d858227904a191fb54420
Bui Duc Nam <doubledev@gmail.com>
2020-12-29 11:52:49 +0700

    afs

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const mapSocketModule = require('./socket/MapSocket')
 const mapSocketMap = {}
 
 dotenv.config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 2000
 app.use(express.static('public'))
 app.use(cors())
 app.use(express.static('uploads'))
